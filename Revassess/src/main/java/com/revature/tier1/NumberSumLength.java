a082819ff07ba60d976ec9127293f9ee48d39b26
Eurus Wagner <euruswagner@gmail.com>
2020-08-10 15:18:00 -0400

    Everything passes

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,14 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+		String number = String.valueOf(num);
+		char [] digits = number.toCharArray();
+		long sum = 0;
+		for (int i = 0; i < digits.length; i++) {
+			int currentDigit = Character.getNumericValue(digits[i]);
+			long currentDigitRaised = (long) Math.pow(currentDigit, digits.length);
+			sum += currentDigitRaised;
+		}
+		return sum == num;
 	}
 }
